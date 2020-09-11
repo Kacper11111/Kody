a2664c514ba6a53e7f5b6b3d81a9971da2ac901d
UwolnicBarabasza <kacper.niewiadomski@lo1.sandomierz.pl>
2020-09-11 09:12:36 +0200

    dodanie plików źródłowych cpp i pythona

---
+++ python/pole_kola.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  bez nazwy.py
+
+def main(args):
+    r = input("Podaj promień: ")
+    pole = r * r * 3.14
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
