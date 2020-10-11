b49e185a227828373b1384e0459f74716c91c64b
UwolnicBarabasza <kacper.niewiadomski@lo1.sandomierz.pl>
2020-10-11 23:16:38 +0200

    Porządek w folderach

--- Petle/petle_cw4.py
+++ Petle/petle_cw4.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  petle_cw2.py
+#  petle_cw4.py
 #  
-#  Copyright 2020  <>
+#  Copyright 2020 Karol <Karol@DESKTOP-EDM589B>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -24,18 +24,20 @@
 
 
 def main(args):
-    
-    n = int(input("Podaj początek zakresu: "))
-    m = int(input("Podaj koniec zakresu: "))
-    
-    
-    
-    
-    
-    
-    
-    return 0
+	
+	m=int(input("Podaj liczbę początkową: "))
+	n=int(input("Podaj liczbę końcową: "))
+	liczba = 0
+	
+	for liczba in range(m, n):
+		if liczba >= 10 and liczba <=100:
+			if liczba % 2 == 0:  
+				if liczba % 3 == 0:  
+					print(liczba)
+
+	return 0
 
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
+
